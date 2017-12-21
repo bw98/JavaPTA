c389783c11a9962064779de3704181807094fa1a
WeiFeng Lai <786016746@qq.com>
2017-12-21 13:27:42 +0800

    Update RegularPentagon.java
    更新了计算正n边形的面积公式

--- RegularPentagon.java
+++ RegularPentagon.java
@@ -15,7 +15,8 @@ class RPentagon extends shape {
     }
     @Override
     public double getArea () {
-        return 5*Math.pow(this.e,2)/(4*Math.tan(36*Math.PI/180));
+        return 5*e*e/(4*Math.tan(36*Math.PI/180)); //计算正n边形的面积公式为： Area=n×a×a/(tan((180度/n))×4);
+        //Math.tan() 的参数是弧度形式，角度弧度互换公式: 弧度 = 角度*(Math.PI/180)
     }
     @Override
     public double getPerimeter () {
