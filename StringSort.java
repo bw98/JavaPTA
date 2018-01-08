4f6e9a07e272aac8ab2788c385dd001a9ec6f8f4
WeiFeng Lai <786016746@qq.com>
2018-01-08 22:57:13 +0800

    Update and rename stringSort.java to StringSort.java

--- StringSort.java
+++ StringSort.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class stringSort {
+public class StringSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String s = input.next();
         char[] charArray = s.toCharArray();
-        Arrays.sort(charArray);
+        Arrays.sort(charArray); //利用 Arrays 类的静态方法 sort 进行排序
         System.out.println(charArray);
 
     }
