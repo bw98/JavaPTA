859d2bdb43ff3e5a21157f4c85b36e0e2704b60d
WeiFeng Lai <786016746@qq.com>
2018-01-08 22:55:01 +0800

    Update and rename stringReplace.java to StringReplace.java

--- StringReplace.java
+++ StringReplace.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class stringReplace {
+public class StringReplace {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String s = input.next();
