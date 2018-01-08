69d7ac19189c19b8382c3340450b07529a151aef
WeiFeng Lai <786016746@qq.com>
2018-01-08 22:37:38 +0800

    Update and rename isNumber.java to IsNumber.java

--- IsNumber.java
+++ IsNumber.java
@@ -1,6 +1,16 @@
+/*
+7-1 给定一个字符串，判定是否是数字（10 分）
+输出格式:
+如果是数字就输出true , 不是数字就输出false.
+
+输入样例:
+3.145
+输出样例:
+true
+*/
 import java.util.Scanner;
 
-public class isNumber {
+public class IsNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String s = new String(input.next());
